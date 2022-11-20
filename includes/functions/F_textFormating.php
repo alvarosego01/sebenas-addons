386e3c561f7e38accd8a75699d716b3c0d1da39e
Alvaro Segovia <alvarosego01@gmail.com>
2022-11-19 20:29:44 -0400

    Progreso ajuste de colores, formas y mobile

--- includes/functions/F_textFormating.php
+++ includes/functions/F_textFormating.php
@@ -1,9 +1,8 @@
 <?php
 
-if (! defined('ABSPATH')) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
-
 
 if (!class_exists('F_textFormating')) {
     class F_textFormating
@@ -29,6 +28,22 @@ if (!class_exists('F_textFormating')) {
                     if (str_contains($aux_text, '{{p') && str_contains($aux_text, 'p}}')) {
                         $aux_text = str_replace('{{p', '<span class="primaryColor">', $aux_text);
                         $aux_text = str_replace('p}}', '</span>', $aux_text);
+                    }
+                    if (str_contains($aux_text, '{{yellow') && str_contains($aux_text, 'yellow}}')) {
+                        $aux_text = str_replace('{{yellow', '<span class="yellowColor">', $aux_text);
+                        $aux_text = str_replace('yellow}}', '</span>', $aux_text);
+                    }
+                    if (str_contains($aux_text, '{{green') && str_contains($aux_text, 'green}}')) {
+                        $aux_text = str_replace('{{green', '<span class="greenColor">', $aux_text);
+                        $aux_text = str_replace('green}}', '</span>', $aux_text);
+                    }
+                    if (str_contains($aux_text, '{{dblue') && str_contains($aux_text, 'dblue}}')) {
+                        $aux_text = str_replace('{{dblue', '<span class="darkBlueColor">', $aux_text);
+                        $aux_text = str_replace('dblue}}', '</span>', $aux_text);
+                    }
+                    if (str_contains($aux_text, '{{white') && str_contains($aux_text, 'white}}')) {
+                        $aux_text = str_replace('{{white', '<span class="whiteColor">', $aux_text);
+                        $aux_text = str_replace('white}}', '</span>', $aux_text);
                     }
                 }
             }
