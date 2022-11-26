4e7d5e768e1fb73ed27cb4c3a6ef69a454de568b
Alvaro Segovia <alvarosego01@gmail.com>
2022-11-25 22:17:15 -0400

    *

--- includes/plugin.php
+++ includes/plugin.php
@@ -21,7 +21,7 @@ final class Plugin
      *
      * @var string The addon version.
      */
-    const VERSION = '1.200.325';
+    const VERSION = '1.200.328';
 
     /**
      * Minimum Elementor Version.
