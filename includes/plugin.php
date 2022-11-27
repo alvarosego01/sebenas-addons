d4a71c4ae321180742d97ef864de323b5e6d99b6
Alvaro Segovia <alvarosego01@gmail.com>
2022-11-26 21:43:00 -0400

    ajustes responsivos

--- includes/plugin.php
+++ includes/plugin.php
@@ -21,7 +21,7 @@ final class Plugin
      *
      * @var string The addon version.
      */
-    const VERSION = '1.200.328';
+    const VERSION = '1.200.330';
 
     /**
      * Minimum Elementor Version.
